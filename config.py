c822d0b364bc337d517681128d63e532a337cb95
Caio César <caiooliveira3652@outlook.com>
2021-06-10 13:57:57 -0300

    alter config

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
-API_Key = 'Sla3fwnFVnSPA8ZL2vAqHpEQl'
-API_Secret_Key = '7DZUl9Yq6JejH0fIKnjhSKlC1JxOisyj0LUPdtsxLFpk4fRMm9'
-Bearer_Token = 'AAAAAAAAAAAAAAAAAAAAAPslQgEAAAAAuvCYEd5KYA%2BI2FAUwacsNeJTz0A%3DBG3pGZkuUnjaqsVkSdC9ygNbt6RDU2X7wGoCZuhhfCFVkrm76W'
+########===== TWITTER =====########
+API_Key = ''
+API_Secret_Key = ''
+Bearer_Token = ''
 
-Access_Token = '571915942-v4g9xkxL2EpjfLj9u6ROCWYxzXuXlAYDZj70e558'
-Access_Token_Secret = 'MTXeSog4X3NTbS6rsdRQKTnI4TLO9FvEr311wqh4yMQx2'
+Access_Token = ''
+Access_Token_Secret = ''
 
-########===== MONGO ====#######
-CON = "mongodb+srv://cof:231456@cluster0.shfk9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+########===== MONGO =====########
+CONN = ""
